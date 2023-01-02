1f43c3b3c2bf83d792dad9bb47e6ad02641a4a57
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:15:18 +0000

    feat: add calories class
    1. Retrieve weekly calories from spreadsheet.
    2. Update weekly calories from spreadsheet.

---
+++ calories.py
@@ -0,0 +1,9 @@
+class Calories:
+    """Calorie  class"""
+
+    def __init__(self, date, week, data):
+        # Initialise class variables
+        self.date = date
+        self.week = week
+        self.data = data
+
