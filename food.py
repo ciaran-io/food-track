4538ff7ed8fc2106d152e26313cbbcd77202058e
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:33:58 +0000

    feat: add validation
    1. Add validation for user input, displays error to
    user if input is not valid.

--- food.py
+++ food.py
@@ -19,3 +19,29 @@ class Food:
             if self.validate_data(values=data_str):
                 print('Adding new food data..')
                 return [self.food, int(self.calories), int(self.weight)]
+
+    def validate_data(self, values) -> bool:
+        """ Validate the food data """
+        try:
+            self.food, self.calories, self.weight = values
+            # Check if calories and weight are positive
+            if int(self.calories) <= 0 or int(self.weight) < 0:
+                raise ValueError("Calories and weight must be a positive number")
+
+            # check if calories and  are less than 5000 and weight less than 1000
+            if int(self.calories) > 5000 or int(self.weight) > 1000:
+                raise ValueError("Calories and weight must be less than 5000 and 1000")
+
+            # Check if calories and weight are integers
+            elif not self.calories.isdigit() or not self.weight.isdigit():
+                raise ValueError("Calories and weight must be a number")
+
+            # Check food is not empty and string
+            elif len(self.food) < 1 or self.food.isnumeric() or len(self.food) > 20:
+                raise ValueError("Food must not be empty and must be a character, \n"
+                                 " food must be less than 20 characters")
+            else:
+                return True
+
+        except ValueError as e:
+            print(f"\nInvalid input: {e}: \n please try again")
