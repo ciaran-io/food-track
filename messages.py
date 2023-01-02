685e5ff2697e1e0a4937ad6b4ba792d2f437f872
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:23:36 +0000

    feat: add welcome message

---
+++ messages.py
@@ -0,0 +1,8 @@
+def welcome_message():
+    """ Welcome message  """
+
+    message_separator = "\n-----------------------------------------\n"
+    welcome = "Welcome to Food Track.\n"
+    welcome += "\n Track all daily calories & foods.\n"
+    welcome += " Select an option from the menu below."
+    print(f"{message_separator} {welcome} {message_separator}")
