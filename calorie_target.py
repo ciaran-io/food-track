df68ddf241f9705a019ae2da98afa2d7df3f765a
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:04:33 +0000

    feat: set/update weekly target
    1. Update weekly calorie target or set target if no
    target is set.

--- calorie_target.py
+++ calorie_target.py
@@ -6,3 +6,27 @@ class CalorieTarget:
         self.target = target
         self.week = week
         self.food_week_data = food_week_data
+
+    def set_week_target(self):
+        """ Set the weekly target """
+
+        while True:
+            if self.validate_target():
+                break
+
+        # Update the target in the Google sheet if not already set
+        if not self.food_week_data.find(str(self.week)):
+            self.food_week_data.append_row([self.week, None, self.target])
+            print(f"Target set to {self.target}")
+
+        else:
+            data_string = input("Target already set. Update? (y/n): ")
+
+            # validate input is y or n keep in loop until valid input
+            while data_string.lower() not in ('y', 'n'):
+                data_string = input("Please enter y to update or n to cancel: ")
+
+            if data_string == 'y':
+                CalorieTarget.update_week_target(self)
+            elif data_string == 'n':
+                print("Target not updated")
