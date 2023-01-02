6bbf9e95396e75da87d175cf4f049752acf2318e
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:32:31 +0000

    feat: create food  from user input.
    1. Create new food with food, calories & food weight.

--- food.py
+++ food.py
@@ -4,3 +4,18 @@ class Food:
         self.calories = calories
         self.weight = weight
 
+    def create_food(self) -> list:
+        """ Create a new food entry for the day """
+
+        while True:
+            message = "\n Add food; Enter your food followed by calories " \
+                      "& weight(optional): \n e.g. Cheese, 273, 48 (comma separated) \n "
+            data_str = input(message).split(',')
+            data_str = [x.lstrip() for x in data_str]  # remove leading spaces
+
+            if len(data_str) < 3:  # Add 0 to values if 2 values are entered
+                data_str.append('0')
+
+            if self.validate_data(values=data_str):
+                print('Adding new food data..')
+                return [self.food, int(self.calories), int(self.weight)]
