43264616fd9d3bcc2df0ef52a8b4d41ebb67791c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:17:15 +0000

    feat: retrieve total calories by date.
    1. Retrieve total calories by date from spreadsheet.

--- calories.py
+++ calories.py
@@ -7,3 +7,20 @@ class Calories:
         self.week = week
         self.data = data
 
+    def get_cells_by_date(self) -> list:
+        """ Get cells from google sheet by date """
+        cell_list = self.data.findall(self.date)
+        # print("retrieving foods..")
+        return cell_list
+
+    def get_total_calories_by_date(self) -> int:
+        """ Get the total calorie amount by date """
+
+        total_weight = 0
+        cell_list = self.get_cells_by_date()
+
+        for cell in cell_list:
+            row = cell.row
+            total_weight += int(self.data.cell(row, 3).value)
+
+        return total_weight
