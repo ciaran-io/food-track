8045ddd0c4ad54b4d7fcbd4f1d1b53090b87c6d7
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 21:44:24 +0000

    feat: add food information to google sheet
    1. Create new food entry, food, calories, weight.
    2. Add/Update weekly calories.

--- user_options.py
+++ user_options.py
@@ -3,3 +3,19 @@ class UserOptions:
 
     def __init__(self, user_input):
         self.user_input = user_input
+
+    def user_options(self):
+        """Check user input and run the relevant function"""
+        
+        match self.user_input:
+            case '1':
+                # Create new food entry in google sheets
+                new_food = Food().create_food()
+                new_food.insert(0, str(today_date))  # insert today's date at start of list
+                foods_sheet.append_row(new_food)
+                print('Food data added successfully')
+
+                # Update the weekly calories
+                Calories(date=str(today_date), week=current_week, data=foods_week_sheet) \
+                    .update_weekly_calories(calories=int(new_food[2]))
+
