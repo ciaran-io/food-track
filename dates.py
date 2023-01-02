8afc9a0d78ea45d1f835424f6fdf7abc1d18c75c
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 18:42:22 +0000

    feat: add dates
    1. Add dates for current day, current week & week number.

---
+++ dates.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+# Dates
+today_date = dt.date.today()
+current_week = today_date.isocalendar()[1]
+
+# Create dates for the week
+monday = today_date - dt.timedelta(days=today_date.weekday())
+sunday = monday + dt.timedelta(days=6)
+week_dates = [monday, monday + dt.timedelta(days=1), monday + dt.timedelta(days=2),
+              monday + dt.timedelta(days=3), monday + dt.timedelta(days=4),
+              monday + dt.timedelta(days=5), monday + dt.timedelta(days=6)]
