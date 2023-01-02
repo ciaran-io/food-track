d5b8398c882d03928193f27e34e8a1251a28f2bd
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 19:46:19 +0000

    feat: create daily food table
    1. Create table for to display daily food breakdown,
    for retrieved data from google spread sheets.

--- pretty_table_data.py
+++ pretty_table_data.py
@@ -17,3 +17,34 @@ for option in range(len(available_options)):
 
 # Align the description column to the left
 food_options_table.align["description"] = "l"
+
+# Create a daily food table
+food_table = PrettyTable()
+food_table.field_names = ["Food", "Calories", "Weight(g)", "Target", "Remaining", "Percentage"]
+
+def create_daily_food_table(food_data, foods_sheet, target):
+    """ Create a table of food from a list of food objects """
+    print('Creating daily food table...')
+    remaining_calories = target
+
+    for food in food_data:
+        row = food.row
+
+        # Reduce the remaining calories by the calories of the food
+        # if target is 0 then remaining calories will be 0
+        if remaining_calories != 0:
+            remaining_calories -= int(foods_sheet.cell(row, 3).value)
+        else:
+            remaining_calories = 0
+
+        percentage = return_percentage(remaining_calories, target)
+
+        # Add the data to the table
+        food_table.add_row([foods_sheet.cell(row, 2).value, foods_sheet.cell(row, 3).value,
+                            foods_sheet.cell(row, 4).value, target, remaining_calories, percentage])
+
+    print(food_table)
+    # Reset the table to prevent duplicate data
+    food_table.clear_rows()
+
+
