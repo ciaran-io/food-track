daed7bbf3550c173ef181deca3a2237abdd2073b
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-02 21:58:24 +0000

    feat: add main application functions
    1. Add welcome message.
    2. Add options menu table to display available options to user.
    3. Add user input class to handle user input/validation.

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from messages import welcome_message
+from pretty_table_data import food_options_table
+from user_options import UserOptions
+
+welcome_message()
+print(food_options_table)
+
+def main():
+    """ Run all program functions """
+    while True:
+        user_input = input("\nPlease choose an option: ")
+        UserOptions(user_input).user_options()
+
+
+if __name__ == "__main__":
+    main()
